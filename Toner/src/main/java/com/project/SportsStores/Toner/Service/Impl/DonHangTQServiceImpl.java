ae3939f2b9dc5977434415ab9a1193e78569ecb6
ADMIN\Dell <vannvph28656@fpt.edu.vn>
2024-02-22 22:21:49 +0700

    add hoa don cho

---
+++ Toner/src/main/java/com/project/SportsStores/Toner/Service/Impl/DonHangTQServiceImpl.java
@@ -0,0 +1,44 @@
+package com.project.SportsStores.Toner.Service.Impl;
+
+import com.project.SportsStores.Toner.Model.DonHang;
+import com.project.SportsStores.Toner.Repository.DonHangRepository;
+import com.project.SportsStores.Toner.Service.DonHangTQService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DonHangTQServiceImpl implements DonHangTQService {
+
+    @Autowired
+    private DonHangRepository dhrp;
+
+    @Override
+    public List<DonHang> getALL() {
+        return dhrp.findAll();
+    }
+
+    @Override
+    public boolean save(DonHang donHang) {
+        dhrp.saveAndFlush(donHang);
+        return true;
+    }
+
+    @Override
+    public List<DonHang> getAllByStatus() {
+        return dhrp.getByStatus();
+    }
+
+    @Override
+    public Page<DonHang> pageOfDonHang(Pageable pageable) {
+        return dhrp.findAll(pageable);
+    }
+
+    @Override
+    public Page<DonHang> seacrhSellOff(String keyword, Pageable pageable) {
+        return dhrp.searchDonHang(keyword,pageable);
+    }
+}
