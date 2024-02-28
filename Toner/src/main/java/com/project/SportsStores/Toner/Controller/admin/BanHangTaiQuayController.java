4b83fa1cba96ff4f65af4ffe2fa154c86a67a412
hieucv25 <chuhieu05@gmail.com>
2024-02-28 12:21:53 +0700

    add toast-message

--- Toner/src/main/java/com/project/SportsStores/Toner/Controller/admin/BanHangTaiQuayController.java
+++ Toner/src/main/java/com/project/SportsStores/Toner/Controller/admin/BanHangTaiQuayController.java
@@ -16,8 +16,6 @@ import com.project.SportsStores.Toner.Repository.NhanVienRepository;
 import com.project.SportsStores.Toner.Service.Impl.DonHangTQServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
