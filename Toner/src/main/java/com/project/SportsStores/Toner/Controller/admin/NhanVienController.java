4c6453d6cf08e41d1279f7ad33731191014b319b
Khanhhq-Ph20927 <khanhhqph20927@fpt.edu.vn>
2024-03-02 22:43:07 +0700

    Update validate

--- Toner/src/main/java/com/project/SportsStores/Toner/Controller/admin/NhanVienController.java
+++ Toner/src/main/java/com/project/SportsStores/Toner/Controller/admin/NhanVienController.java
@@ -3,6 +3,7 @@ package com.project.SportsStores.Toner.Controller.admin;
 import com.project.SportsStores.Toner.Model.NhanVien;
 import com.project.SportsStores.Toner.Service.ChucVuService;
 import com.project.SportsStores.Toner.Service.NhanVienService;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -42,47 +46,62 @@ public class NhanVienController {
 
     @PostMapping("/add")
     public String add(Model model, RedirectAttributes redirectAttributes, @ModelAttribute("nhanVien") NhanVien nhanVien) {
+        boolean isValid = false;
         model.addAttribute("chucVu", chucVuService.getAll());
         for (NhanVien nv : service.getAll()) {
             nhanVien.setMaNV("NV" + service.getAll().size());
             if (nhanVien.getMaNV().equalsIgnoreCase(nv.getMaNV())) {
                 nhanVien.setMaNV("NV" + (service.getAll().size() + 1));
             }
+            if (nhanVien.getSdt().equalsIgnoreCase(nv.getSdt())) {
+                isValid = true;
+                model.addAttribute("errorPhone", "Số điện thoại trùng !");
+            }
+            if (nhanVien.getEmail().equalsIgnoreCase(nv.getEmail())) {
+                isValid = true;
+                model.addAttribute("errorEmail", "Email trùng !");
+            }
         }
         nhanVien.setMatKhau("12345");
         nhanVien.setNgayTao(LocalDateTime.now());
-        boolean isValid = false;
         if (nhanVien.getHoTen().isEmpty()) {
             isValid = true;
-            model.addAttribute("errorName", "Please Choose Name");
+            model.addAttribute("errorName", "Họ tên trống !");
+        } else if (!nhanVien.getHoTen().matches("^([AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬBCDĐEÈẺẼÉẸÊỀỂỄẾỆFGHIÌỈĨÍỊJKLMNOÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢPQRSTUÙỦŨÚỤƯỪỬỮỨỰVWXYỲỶỸÝỴZ][aàảãáạăằẳẵắặâầẩẫấậbcdđeèẻẽéẹêềểễếệfghiìỉĩíịjklmnoòỏõóọôồổỗốộơờởỡớợpqrstuùủũúụưừửữứựvwxyỳỷỹýỵz]+)((\\s{1}[AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬBCDĐEÈẺẼÉẸÊỀỂỄẾỆFGHIÌỈĨÍỊJKLMNOÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢPQRSTUÙỦŨÚỤƯỪỬỮỨỰVWXYỲỶỸÝỴZ][aàảãáạăằẳẵắặâầẩẫấậbcdđeèẻẽéẹêềểễếệfghiìỉĩíịjklmnoòỏõóọôồổỗốộơờởỡớợpqrstuùủũúụưừửữứựvwxyỳỷỹýỵz]+){1,})$")) {
+            isValid = true;
+            model.addAttribute("errorName", "Họ tên không đúng định dạng !");
         }
         if (nhanVien.getSdt().isEmpty()) {
             isValid = true;
-            model.addAttribute("errorPhone", "Please Choose Phone Number");
+            model.addAttribute("errorPhone", "Số điện thoại trống !");
         } else if (!nhanVien.getSdt().matches("(84|0[3|5|7|8|9])+([0-9]{8})\\b")) {
             isValid = true;
-            model.addAttribute("errorPhone", "Please Regex Phone Number");
+            model.addAttribute("errorPhone", "Số điện thoại không đúng định dạng !");
         }
         if (nhanVien.getEmail().isEmpty()) {
             isValid = true;
-            model.addAttribute("errorEmail", "Please Choose Email");
-        } else if (!nhanVien.getEmail().matches(".+@.+\\.+.+")) {
+            model.addAttribute("errorEmail", "Email trống !");
+
+        } else if (!nhanVien.getEmail().matches("^(([a-zA-Z0-9\\._-]+)@([a-zA-Z0-9-]+)\\.([a-zA-Z]{2,4})(\\.([a-zA-Z]{2,4})|))$")) {
             isValid = true;
-            model.addAttribute("errorEmail", "Please Regex Email");
+            model.addAttribute("errorEmail", "Email không đúng định dạng !");
         }
-        if (nhanVien.getNgaySinh() == null) {
+        if (nhanVien.getNgaySinh()==null) {
             isValid = true;
-            model.addAttribute("errorBirthday", "Please Choose  Phone Number");
+            model.addAttribute("errorBirthday", "Ngày sinh trống !");
         }
-        if (nhanVien.getCv().getId() == 0) {
+//        else if () {
+//            isValid = true;
+//            model.addAttribute("errorBirthday", "Ngày sinh trống");
+//        }
+        if (nhanVien.getCv() == null) {
             isValid = true;
-            model.addAttribute("errorPosition", "Please Choose  Phone Number");
+            model.addAttribute("errorPosition", "Chưa chọn chức vụ !");
         }
-        if (isValid==false) {
+        if (isValid) {
             service.save(nhanVien);
             redirectAttributes.addFlashAttribute("message", true);
-            return "redirect:/api/admin/staff/add";
-//            return "admin/staff/staff-add";
+            return "redirect:/admin/staff/add";
         } else {
             model.addAttribute("message", false);
             return "admin/staff/staff-add";
@@ -95,7 +114,7 @@ public class NhanVienController {
     }
 
     @PostMapping("/update/{id}")
-    public String update(@PathVariable(value = "id") Long id,RedirectAttributes redirectAttributes
+    public String update(@PathVariable(value = "id") Long id, RedirectAttributes redirectAttributes
             , @ModelAttribute("nhanVien") NhanVien nhanVien) {
         NhanVien updateNV = service.findById(id).get();
         updateNV.setHoTen(nhanVien.getHoTen());
@@ -107,7 +126,7 @@ public class NhanVienController {
 //        updateNV.setAnhNhanVien(nhanVien.getAnhNhanVien());
         service.save(nhanVien);
         redirectAttributes.addFlashAttribute("message", true);
-        return "redirect:/api/admin/staff";
+        return "redirect:/admin/staff";
 //        return "admin/staff/staff-list";
     }
 
