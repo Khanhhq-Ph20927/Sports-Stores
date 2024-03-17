c77330f78baf295a2b6cf08f7f9c53035d278c4b
Khanhhq-Ph20927 <khanhhqph20927@fpt.edu.vn>
2024-03-17 16:04:10 +0700

    Merge Van15

--- Toner/src/main/java/com/project/SportsStores/Toner/repository/ThuongHieuRepository.java
+++ Toner/src/main/java/com/project/SportsStores/Toner/repository/ThuongHieuRepository.java
@@ -1,13 +1,24 @@
 package com.project.SportsStores.Toner.Repository;
 
 import com.project.SportsStores.Toner.Model.ThuongHieu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-@Repository
 public interface ThuongHieuRepository extends JpaRepository<ThuongHieu,Long> {
     @Override
+    <S extends ThuongHieu> S save(S entity);
+
+    @Override
     Optional<ThuongHieu> findById(Long aLong);
+
+    @Override
+    Page<ThuongHieu> findAll(Pageable pageable);
+
+    @Query("Select th from ThuongHieu th WHERE th.ten LIKE %:keyword% or th.email like  %:keyword% ")
+    Page<ThuongHieu> searchTH(@Param("keyword") String keyword, Pageable pageable);
 }
